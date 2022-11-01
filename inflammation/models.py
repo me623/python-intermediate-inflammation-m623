4ca35f4772101d3370f3b020dca89c5745307fa5
me623 <morganjedwards01@gmail.com>
2022-11-01 12:55:09 +0000

    fixed whitespace issues

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row 
-contains inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row
+contains inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -21,7 +21,7 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-    
+
     :param data: 2D array for mean calculation
     :return: mean value of day column
     """
@@ -30,7 +30,7 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    
+
     :param data: 2D array for max calulation
     :return: max value of day column
     """
@@ -39,10 +39,9 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    
+
     :param data: 2D array for max calulation
     :return: min value of day column
-    :return: min value of day 
+    :return: min value of day
     """
     return np.min(data, axis=0)
-
