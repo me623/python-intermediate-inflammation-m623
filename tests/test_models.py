bdf7a8252b0637d0efd79dbffd9ef9e0bf27c774
me623 <morganjedwards01@gmail.com>
2022-11-02 10:12:00 +0000

    added tests for models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,28 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_integers():
+    "test the max function works for an array of positive integers"
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([5, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_integers():
+    "test the max function works for an array of positive integers"
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
