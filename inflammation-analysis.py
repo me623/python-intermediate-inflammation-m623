90b5374e3fa108b9a06cd921cb85b994d095f30b
me623 <morganjedwards01@gmail.com>
2022-11-02 09:42:35 +0000

    start of day 2

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -18,15 +18,15 @@ def main(args):
     if not isinstance(in_files, list):
         in_files = [args.infiles]
 
-
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 
-                    'max': models.daily_max(inflammation_data), 
-                    'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
